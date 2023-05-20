bd9904cbeac78c30dd7363581ce8753d5053d34c
Niklas Weimann <nikweimann@gmail.com>
2023-05-20 10:53:29 +0200

    Update examples to .net7

--- InlineKeyboard/InlineKeyboard.cs
+++ InlineKeyboard/InlineKeyboard.cs
@@ -3,75 +3,60 @@ using System.Threading.Tasks;
 using RxTelegram.Bot.Interface.BaseTypes;
 using RxTelegram.Bot.Interface.BaseTypes.Requests.Callbacks;
 using RxTelegram.Bot.Interface.BaseTypes.Requests.Messages;
+using RxTelegram.Bot.Utils.Keyboard;
 
-namespace RxTelegram.Bot.Examples.InlineKeyboard
+namespace RxTelegram.Bot.Examples.InlineKeyboard;
+
+internal static class InlineKeyboard
 {
-    internal static class InlineKeyboard
+    private static TelegramBot Bot { get; set; }
+    private const string BotToken = "<PASTE YOUR BOT TOKEN HERE>";
+
+    public static async Task Main()
     {
-        private static TelegramBot Bot { get; set; }
-        private const string BotToken = "<PASTE YOUR BOT TOKEN HERE>";
+        Bot = new TelegramBot($"{BotToken}");
+        var me = await Bot.GetMe();
+        Console.WriteLine($"Bot name: @{me.Username}");
 
-        public static async Task Main()
+        var subscriptionMessage = Bot.Updates.Message.Subscribe(HandleReceivedMessage,
+            exception => Console.WriteLine($"An error has occured: {exception.Message}"));
+        var subscriptionCallbackQuery = Bot.Updates.CallbackQuery.Subscribe(HandleCallbackQuery,
+            exception => Console.WriteLine($"An error has occured: {exception.Message}"));
+        Console.ReadLine();
+        subscriptionMessage.Dispose();
+        subscriptionCallbackQuery.Dispose();
+    }
+
+    private static void HandleCallbackQuery(CallbackQuery callbackQuery)
+    {
+        Bot.AnswerCallbackQuery(new AnswerCallbackQuery
         {
-            Bot = new TelegramBot($"{BotToken}");
-            var me = await Bot.GetMe();
-            Console.WriteLine($"Bot name: @{me.Username}");
+            Text = callbackQuery.Data,
+            CallbackQueryId = callbackQuery.Id
+        });
+    }
 
-            var subscriptionMessage = Bot.Updates.Message.Subscribe(HandleReceivedMessage,
-                exception => Console.WriteLine($"An error has occured: {exception.Message}"));
-            var subscriptionCallbackQuery = Bot.Updates.CallbackQuery.Subscribe(HandleCallbackQuery,
-                exception => Console.WriteLine($"An error has occured: {exception.Message}"));
-            Console.ReadLine();
-            subscriptionMessage.Dispose();
-            subscriptionCallbackQuery.Dispose();
-        }
-
-        private static void HandleCallbackQuery(CallbackQuery callbackQuery)
+    private static void HandleReceivedMessage(Message message)
+    {
+        if (message.Text.Length != 0)
         {
-            Bot.AnswerCallbackQuery(new AnswerCallbackQuery
+            Bot.SendMessage(new SendMessage
             {
-                Text = callbackQuery.Data,
-                CallbackQueryId = callbackQuery.Id
-            });
-        }
-        
-        private static void HandleReceivedMessage(Message message)
-        {
-            if (message.Text.Length != 0)
-            {
-                Bot.SendMessage(new SendMessage
+                ChatId = message.Chat.Id,
+                Text = "Here is a inline keyboard!",
+                ReplyMarkup = new InlineKeyboardMarkup
                 {
-                    ChatId = message.Chat.Id,
-                    ReplyMarkup = new InlineKeyboardMarkup
-                    {
-                        InlineKeyboard = new[]
-                        {
-                            new[]
-                            {
-                                new InlineKeyboardButton
-                                {
-                                    CallbackData = "You clicked on Abc",
-                                    Text = "Abc"
-                                },
-                            },
-                            new[]
-                            {
-                                new InlineKeyboardButton
-                                {
-                                    CallbackData = "You clicked on 1",
-                                    Text = "1"
-                                },
-                                new InlineKeyboardButton
-                                {
-                                    CallbackData = "You clicked on 2",
-                                    Text = "2"
-                                }
-                            }
-                        }
-                    },
-                    Text = "Here is a inline keyboard!"
-                });
-            }
+                    InlineKeyboard = KeyboardBuilder.CreateInlineKeyboard()
+                        .BeginRow()
+                        .AddCallbackData("Abc", "You clicked on Abc")
+                        .EndRow()
+                        .BeginRow()
+                        .AddCallbackData("1", "You clicked on 1")
+                        .AddCallbackData("2", "You clicked on 2")
+                        .EndRow()
+                        .Build()
+                }
+            });
         }
     }
 }
--- ReceiveFiles/ReceiveFiles.cs
+++ ReceiveFiles/ReceiveFiles.cs
@@ -7,55 +7,55 @@ using RxTelegram.Bot.Interface.BaseTypes;
 using RxTelegram.Bot.Interface.BaseTypes.Requests.Messages;
 using File = System.IO.File;
 
-namespace RxTelegram.Bot.Examples.ReceiveFiles
+namespace RxTelegram.Bot.Examples.ReceiveFiles;
+
+internal static class ReceiveFiles
 {
-    internal static class ReceiveFiles
+    private static TelegramBot Bot { get; set; }
+    private const string BotToken = "<PASTE YOUR BOT TOKEN HERE>";
+
+    public static async Task Main()
     {
-        private static TelegramBot Bot { get; set; }
-        private const string BotToken = "<PASTE YOUR BOT TOKEN HERE>";
+        Bot = new TelegramBot($"{BotToken}");
+        var me = await Bot.GetMe();
+        Console.WriteLine($"Bot name: @{me.Username}");
 
-        public static async Task Main()
+        var subscription = Bot.Updates.Message.Subscribe(HandleReceivedMessage,
+            exception => Console.WriteLine($"An error has occured: {exception.Message}"));
+        Console.ReadLine();
+        subscription.Dispose();
+    }
+
+    private static void HandleReceivedMessage(Message message)
+    {
+        RxTelegram.Bot.Interface.BaseTypes.File file = null;
+        if (message.Photo != null && message.Photo.Any())
         {
-            Bot = new TelegramBot($"{BotToken}");
-            var me = await Bot.GetMe();
-            Console.WriteLine($"Bot name: @{me.Username}");
-
-            var subscription = Bot.Updates.Message.Subscribe(HandleReceivedMessage,
-                exception => Console.WriteLine($"An error has occured: {exception.Message}"));
-            Console.ReadLine();
-            subscription.Dispose();
+            var photo = message.Photo.MinBy(x => x.Height);
+            if (photo != null) file = Bot.GetFile(photo.FileId).Result;
+        }
+        else if (message.Document != null)
+        {
+            file = Bot.GetFile(message.Document.FileId).Result;
+        }
+        else if (message.Video != null)
+        {
+            file = Bot.GetFile(message.Video.FileId).Result;
+        }
+        else if (message.Audio != null)
+        {
+            file = Bot.GetFile(message.Audio.FileId).Result;
         }
 
-        private static void HandleReceivedMessage(Message message)
+        if (file == null)
         {
-            RxTelegram.Bot.Interface.BaseTypes.File file = null;
-            if (message.Photo != null && message.Photo.Any())
-            {
-                var photo = message.Photo.OrderBy(x => x.Height).FirstOrDefault();
-                if (photo != null) file = Bot.GetFile(photo.FileId).Result;
-            }
-            else if (message.Document != null)
-            {
-                file = Bot.GetFile(message.Document.FileId).Result;
-            }
-            else if (message.Video != null)
-            {
-                file = Bot.GetFile(message.Video.FileId).Result;
-            }
-            else if (message.Audio != null)
-            {
-                file = Bot.GetFile(message.Audio.FileId).Result;
-            }
-
-            if (file == null)
-            {
-                Bot.SendMessage(new SendMessage {ChatId = message.Chat.Id, Text = "Please send me a file!"});
-                return;
-            }
-            var photoData = Bot.DownloadFileByteArray(file).Result;
-            var filename = file.FilePath.Split("/").LastOrDefault();
-            File.WriteAllBytes(
-                Path.Combine(Environment.CurrentDirectory, $"Output/{filename}"), photoData);
+            Bot.SendMessage(new SendMessage { ChatId = message.Chat.Id, Text = "Please send me a file!" });
+            return;
         }
+
+        var photoData = Bot.DownloadFileByteArray(file).Result;
+        var filename = file.FilePath.Split("/").LastOrDefault();
+        File.WriteAllBytes(
+            Path.Combine(Environment.CurrentDirectory, $"Output/{filename}"), photoData);
     }
 }
